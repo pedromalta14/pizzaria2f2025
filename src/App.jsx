37dda0e65ebec0b17d65536e345d59edc0acb2a3
pedromalta14 <rm95182@estudante.fieb.edu.br>
2025-09-18 07:42:00 -0300

    De novo

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,12 @@
-import { useState } from "react"
-import poke from './assets/pokeapi_256.png'
-
-function App() {
-
-
- // var nome = "yuri alberto"
-
- const Formulario = (props) => {
-
-  const [nome, setNome] = useState ("Memphis")
+const App = () => {
 
 
   return (
-    <div>
-      <input 
-        className="nome"
-        name="nome"
-        onChange= {(e)=>{setNome(e.target.value)}}
-        placeholder={props.sombra != null ? props.sombra : "Texto padrão"}
-        type="text"/>
-
-
-        <button 
-        className="botao"
-        onClick={()=>{alert(nome)}}>
-          CLIQUE AQUI        
-        </button>
-    </div>
+  <>
+   <h1> Projeto Pizzaria </h1>
+  </>
   )
- }
 
-    return (
-      <div>
-        <h3>Pizzzaria 2F</h3>
-        <p> texto de paragrafo</p> 
-        <img src={poke} style={{width:200, height:100 }}/>
-        <Formulario sombra= "Digite seu nome..." />
-        <Formulario sombra= "Digite seu email..."/>
-        <Formulario  sombra= "(99)90000-0000"/>
-        <Formulario/>
-        <Formulario/>
-        <Formulario/>
-        <Formulario/>
-        <Formulario/>
-        <Formulario/>
-        <Formulario/>
-        <Formulario/>
-        <Formulario/>
-        <Formulario/>
-
-      </div>
-    )
 }
 
 export default App
- 

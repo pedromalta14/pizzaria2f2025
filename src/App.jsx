286f62570d5882ccdcb29a67ad79f95a3a1b36e4
pedromalta14 <rm95182@estudante.fieb.edu.br>
2025-09-19 07:47:50 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-const App = () => {
+ import Produtos from "./pages/produtos"
+
+ 
+ const App = () => {
 
 
   return (
   <>
    <h1> Projeto Pizzaria </h1>
+   <Produtos/>
   </>
   )
 
---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,19 @@
+
+const Produtos = () => {
+
+return (
+    <div>
+        <ul>
+            <li>Pizza de Muçarela </li>
+            <li>Pizza de Calabresa </li>
+            <li>Pizza de Portuguesa </li>
+            <li>Pizza de Baiana </li>
+
+
+        </ul>
+    </div>
+
+)
+
+}
+export default Produtos 
